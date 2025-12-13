d60caac811d75ea6d9051b04f1191a7ccf1cfd09
wootaiklee <fined000@naver.com>
2025-12-13 14:02:42 +0900

    Add Kakao authentication strategy using Passport.js; implement routes for Kakao login and callback handling in the auth module.

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -1,1 +1,37 @@
-module.exports = () => {};
+const passport = require("passport");
+const { Strategy: KakaoStrategy } = require("passport-kakao");
+const User = require("../models/user");
+
+module.exports = () => {
+  passport.use(
+    new KakaoStrategy(
+      {
+        clientID: process.env.KAKAO_ID,
+        clientSecret: process.env.KAKAO_SECRET,
+        callbackURL: "/auth/kakao/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+        try {
+          const exUser = await User.findOne({
+            where: { snsId: profile.id, provider: "kakao" },
+          });
+          if (exUser) {
+            done(null, exUser);
+          } else {
+            const newUser = await User.create({
+              email: profile._json?.kakao_account?.email,
+              nick: profile.displayName,
+              snsId: profile.id,
+              provider: "kakao",
+            });
+            done(null, newUser);
+          }
+        } catch (error) {
+          console.error(error);
+          return done(error);
+        }
+      }
+    )
+  );
+};
