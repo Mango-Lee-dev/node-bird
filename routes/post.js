002a5742ee34121c6ecdb06c4a24529eeab5b3ab
wootaiklee <fined000@naver.com>
2025-12-21 19:20:05 +0900

    Add post upload functionality; implement routes and controllers for image uploads and post creation, and update main page rendering to include posts from the database.

---
+++ routes/post.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const { uploadPost, afterUploadImage } = require("../controllers/post");
+
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  fs.mkdirSync("uploads");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+router.post("/img", isLoggedIn, upload.single("img"), afterUploadImage);
+const upload2 = multer();
+router.post("/", isLoggedIn, upload.none(), uploadPost);
+
+module.exports = router;
