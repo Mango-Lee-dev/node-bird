d01439bd20d69682f35b183aad3256ae426368ab
wootaiklee <fined000@naver.com>
2026-01-01 17:55:02 +0900

    Add JWT token verification middleware; implement verifyToken function to handle token validation and error responses for expired or invalid tokens.

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     // passport 통해서 로그인했는지 여부
@@ -13,5 +15,27 @@ exports.isNotLoggedIn = (req, res, next) => {
   } else {
     const message = encodeURIComponent("이미 로그인한 상태입니다.");
     res.redirect(`/?error=${message}`);
+  }
+};
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    res.locals.decoded = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    return next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(419).json({
+        code: 419,
+        message: "토큰이 만료되었습니다.",
+      });
+    }
+
+    return res.status(401).json({
+      code: 401,
+      message: "유효하지 않은 토큰입니다.",
+    });
   }
 };
